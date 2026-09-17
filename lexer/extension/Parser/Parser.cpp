9194329e94230fce8c1a015a673bbe8d827f4f6b
Имя Фамилия <nak78nik@gmail.com>
2026-09-17 23:37:49 +0300

    Создание класса для парсинга лексем, которые необходимо преобразовать

---
+++ lexer/extension/Parser/Parser.cpp
@@ -0,0 +1,5 @@
+#include "Parser.h"
+
+#include <string>
+
+using std::string;
---
+++ lexer/extension/Parser/Parser.h
@@ -0,0 +1,14 @@
+#ifndef COMPILER_LITERAL_H
+#define COMPILER_LITERAL_H
+
+#include <string>
+
+using std::string;
+
+class Parser {
+    
+private:
+public:
+};
+
+#endif
