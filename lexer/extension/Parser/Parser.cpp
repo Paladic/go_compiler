b9ebb8c49bd625f8e3a87b815d1c38c25a5a467c
Имя Фамилия <nak78nik@gmail.com>
2026-09-17 23:38:34 +0300

    Добавление парсинга целочисленных значений

--- lexer/extension/Parser/Parser.cpp
+++ lexer/extension/Parser/Parser.cpp
@@ -3,3 +3,59 @@
 #include <string>
 
 using std::string;
+
+long long Parser::Integer(const string& text) {
+    
+    string value = removeLowLines(text); // удаляем _
+
+    // Система счисления
+    int base = 10;
+
+    // индекс символа откуда начинаем
+    size_t start = 0;
+
+    // 
+    if (value.size() >= 2 && value[0] == '0') {
+        
+        char secondSymbol = tolower(value[1]); // опускаем в нижний регистр, чтобы проверять условно только 'b', а не 'b' и 'B'
+
+        // Двоичные числа - 0b1010
+        if (secondSymbol == 'b') {
+            base = 2;
+            start = 2;
+        }
+
+        // Восьмиричные числа - 
+        else if (secondSymbol == 'o') {
+            base = 8;
+            start = 2;
+        }
+
+        // Шестнадцатиричные числа
+        else if (secondSymbol == 'x') {
+            base = 16;
+            start = 2;
+        }
+
+        // старая восьмиричная система исчисления
+        else {
+            base = 8;
+            start = 1;
+        }
+    }
+
+    return std::stoll(value.substr(start), nullptr, base);
+}
+string Parser::removeLowLines(const string& text) {
+
+    string value;
+
+     for (char ch : text) {
+        if (ch != '_') {
+            value += ch;
+        }
+    }
+
+    return value;
+
+}
--- lexer/extension/Parser/Parser.h
+++ lexer/extension/Parser/Parser.h
@@ -8,7 +8,19 @@ using std::string;
 class Parser {
     
 private:
+
+    /// @brief удаляет _ из строки
+    /// @param text строка
+    /// @return строка из которого удалены все _
+    static string removeLowLines(const string& text);
+
 public:
+
+    /// @brief Преобразовывает строку в целочисленный тип данных
+    /// @param text строка
+    /// @return целое число
+    static long long Integer(const string& text);
+    
 };
 
 #endif
